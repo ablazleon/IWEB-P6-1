85e53d13c1683841e53c205e062f148ea5892ce2
Adrián <ablazleon@ono.com>
2018-12-16 14:21:09 +0100

    downloadQuizzes: 2. Create the needed new Actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,5 @@
+import {API} from "../assets/constants/constants";
+
 export function introduceAnswer(currentUserAnswer, indexCurrentQuestion){
     return{
         type: "INTRODUCE_ANSWER",
@@ -20,3 +22,46 @@ export function submit(questions){
 	}
 }
 
+export function fetchState() {
+    return dispatch => {
+        dispatch(fetchStateBegin());
+        return fetch(API)
+            .then(handleErrors)
+            .then(res => res.json())
+            .then(json => {
+                return dispatch(fetchStateSuccess(json))
+            })
+            .catch(error=>{
+                console.log(error);
+                return dispatch(fetchStateFailure(error));
+            });
+    }
+
+}
+
+export function fetchStateBegin() {
+    return { type: 'FETCH_STATE_BEGIN' }
+}
+
+export function fetchStateSuccess(json_received) {
+    return{
+        type: 'FETCH_STATE_SUCCESS',
+        state: json_received
+    }
+}
+
+export function fetchStateFailure(error) {
+    return{
+        type: 'FETCH_STATE_FAILURE',
+        error: error
+    }
+}
+
+// Handle errors
+export function handleErrors(response) {
+    if(!response.ok){
+        console.log("ERROR!: "+ response.status.text);
+        throw Error(response.status.text);
+    }
+    return response;
+}
